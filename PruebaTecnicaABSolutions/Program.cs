d541cdf9f59fec794a4e101335d0735ccaee0a80
rigolinux <rigoberto7567@gmail.com>
2024-04-09 13:39:24 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnicaABSolutions/Program.cs
@@ -0,0 +1,34 @@
+using Syncfusion.Blazor;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToController("Index", "Home");
+
+
+
+app.Run();
